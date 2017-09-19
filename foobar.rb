d3f3d14b50e0fff01a59edfc7460743e259bd274
avneesh1mehta <avneesh1mehta@hotmail.com>
2017-09-18 23:13:10 -0700

    Q1-4 completed

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,21 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    # a to int array, increment elements by 2
+    a = a.map(&:to_i).map{|item| item + 2}
+
+    # find all even elements and return a new array with them
+    a = a.find_all{|item| item % 2 == 0 }
+
+    # remove duplicates
+    a = a.uniq
+
+    # rejects resulting numbers > 10
+    a = a.find_all{|item| item < 11}
+
+    # computes sum
+    sum = a.inject(0){|sum,item| sum + item }
+    return sum
   end
 end
-
-
